1b0c763e2bc6e3ee00d7187751c1d2a9094b4d62
藤井絢斗 <fujiken20c@gmail.com>
2021-10-28 23:17:56 +0900

    fotterの見た目を調整

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,35 +1,48 @@
 <template>
   <footer>
     <p class="paragraph">
-    <span style="font-size: 14pt;">第73回 岐阜大学祭</span><br>
-    <NuxtLink :to="`/`">有志企画</NuxtLink><br>
-    委員会企画<br>
-    ・<NuxtLink :to="`/official/owarai`">お笑いライブ</NuxtLink><br>
-    ・<NuxtLink :to="`/official/gidaicollection`">岐大コレクション</NuxtLink><br>
-    ・<NuxtLink :to="`/official/bingo`">ビンゴ大会</NuxtLink><br>
-    ・<NuxtLink :to="`/official/vr`">バーチャル岐阜大学</NuxtLink><br>
-    同時開催イベント<br>
-    ・つかさ祭り<br>
-    ・<a href="https://www.gifu-u.ac.jp/news/event/2021/10/entry06-11065.html">岐阜大学フェア</a><br>
-    <NuxtLink :to="`/contact`">お問い合わせ</NuxtLink>
+      第73回 岐阜大学祭<br />
+      <NuxtLink :to="`/`">有志企画</NuxtLink><br />
+      委員会企画<br />
+      ・<NuxtLink :to="`/official/owarai`">お笑いライブ</NuxtLink><br />
+      ・<NuxtLink :to="`/official/gidaicollection`">岐大コレクション</NuxtLink
+      ><br />
+      ・<NuxtLink :to="`/official/bingo`">ビンゴ大会</NuxtLink><br />
+      ・<NuxtLink :to="`/official/vr`">バーチャル岐阜大学</NuxtLink><br />
+      同時開催イベント<br />
+      ・つかさ祭り<br />
+      ・<a href="https://www.gifu-u.ac.jp/news/event/2021/10/entry06-11065.html"
+        >岐阜大学フェア</a
+      ><br />
+      <NuxtLink :to="`/contact`">お問い合わせ</NuxtLink>
     </p>
-    <p>
-      <img src="/title.svg" class="logo" alt="" />
-    </p>
+    <div class="logo-wrapper">
+      <p>
+        <img src="/title.svg" class="logo" alt="" />
+      </p>
+    </div>
   </footer>
 </template>
 
 <style scoped>
-footer{
+footer {
   background: #aa47f5;
 }
-p{
-  margin: 1rem ;
-  color:white;
+p {
+  margin: 1rem;
+  color: white;
 }
-p.logo{
+.logo-wrapper {
   text-align: center;
-  height: 2rem;
-  
+}
+.logo {
+  max-width: 50%;
+  margin: 1rem 0;
+}
+a {
+  color: white;
+}
+a:hover {
+  text-decoration: underline;
 }
 </style>
