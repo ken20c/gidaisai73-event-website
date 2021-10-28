212df1844e5c59d166008d4aa087f2b6f254b13b
藤井絢斗 <fujiken20c@gmail.com>
2021-10-28 20:55:48 +0900

    group.descriptionを正しい位置(from SNSリンクみだしの下)に変更

--- pages/groups/_slug.vue
+++ pages/groups/_slug.vue
@@ -9,15 +9,15 @@
         <div>
           <img :src="`/img/groups/${group.id}.png`" alt="" class="main-img" />
         </div>
+        <p>
+          {{ group.description }}
+        </p>
         <div>
           <h2 v-show="group.sns.length > 0" class="top-heading">SNSリンク</h2>
           <div v-for="sns in group.sns" :key="sns.url">
             <a :href="sns.url" class="button btn-primary">{{ sns.service }}</a>
           </div>
         </div>
-        <p>
-          {{ group.description }}
-        </p>
       </div>
     </main>
     <Footer />
