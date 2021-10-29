bb0048821499f8d579d5afe9cd9913b45d3320cf
藤井絢斗 <fujiken20c@gmail.com>
2021-10-29 10:48:03 +0900

    ボタンリンクの記法を修正

--- pages/official/gidaicollection/index.vue
+++ pages/official/gidaicollection/index.vue
@@ -3,8 +3,8 @@
     <Header />
     <main>
       <h1 class="container-lg">岐大コレクション</h1>
-      <button class="btn-primary">
-        <a href="https://gidaicollection.studio.site">公式サイトを開く</a>
+      <button class="btn-primary" onclick="https://gidaicollection.studio.site">
+        公式サイトを開く
       </button>
     </main>
     <Footer />
