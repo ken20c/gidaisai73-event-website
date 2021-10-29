39de1178fd5a14d60c4d95bde806dc27863ce240
藤井絢斗 <fujiken20c@gmail.com>
2021-10-29 10:25:25 +0900

    お笑いのページを編集

--- pages/official/owarai/index.vue
+++ pages/official/owarai/index.vue
@@ -1,9 +1,24 @@
 <template>
   <div>
     <Header />
-    <main>
-      <h1 class="container-lg">お笑いライブ</h1>
+    <main class="container">
+      <h1>お笑いライブ</h1>
+      <div>
+        <img
+          src="/img/bira/owarai-bira.jpg"
+          href="https://www.youtube.com/channel/UCSpjVPFMpWoLAcHD7Rx9KDw"
+          alt=""
+        />
+      </div>
+      <Button class="btn-primary"> YouTube を開く </Button>
     </main>
     <Footer />
   </div>
 </template>
+
+<style scoped>
+img {
+  max-width: 100%;
+  margin-bottom: 0.5rem;
+}
+</style>
