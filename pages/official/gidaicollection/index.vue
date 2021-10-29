78e4213273ce1b7eaeb373ebd0f1c1fb30879cd4
藤井絢斗 <fujiken20c@gmail.com>
2021-10-29 10:43:41 +0900

    officialのindexを編集

--- pages/official/gidaicollection/index.vue
+++ pages/official/gidaicollection/index.vue
@@ -3,6 +3,9 @@
     <Header />
     <main>
       <h1 class="container-lg">岐大コレクション</h1>
+      <button class="btn-primary">
+        <a href="https://gidaicollection.studio.site">公式サイトを開く</a>
+      </button>
     </main>
     <Footer />
   </div>
